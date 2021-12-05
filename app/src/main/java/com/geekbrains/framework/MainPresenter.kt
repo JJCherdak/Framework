e67308a68589ebe15fea85b552de701056aca07b
Kovalev Ivan <j.j.cherdak@gmail.com>
2021-12-05 19:43:21 +0300

    added three function ButtonType

---
+++ app/src/main/java/com/geekbrains/framework/MainPresenter.kt
@@ -0,0 +1,24 @@
+package com.geekbrains.framework
+
+class MainPresenter(
+    private val view: MainView,
+    private val model: CountersModel = CountersModel()
+) {
+
+
+    fun counterClick(type: ButtonType) {
+        val dataFromModel = when (type) {
+            ButtonType.FIRST -> model.next(0)
+            ButtonType.SECOND -> model.next(1)
+            ButtonType.THIRD-> model.next(2)
+        }
+        view.setButtonText(type, dataFromModel.toString())
+    }
+}
+
+    enum class ButtonType {
+        FIRST,
+        SECOND,
+        THIRD
+
+    }
